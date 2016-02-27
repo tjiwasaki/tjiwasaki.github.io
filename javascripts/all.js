8ccb041e3c5a39c38128b36ff35cb813440ed2ac
tjiwasaki <thomas@ultravioletglass.com>
2016-02-27 14:11:21 -0800

    first commit

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+
+$('#btnClick').on('click',function(){
+    if($('#1').css('display')!='none'){
+    $('#2').html('Here is my dynamic content').show().siblings('div').hide();
+    }else if($('#2').css('display')!='none'){
+        $('#1').show().siblings('div').hide();
+    }
+});
